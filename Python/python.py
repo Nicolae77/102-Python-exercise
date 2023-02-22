753a0ba94442c8beb98432a5f8c972fe15bd3b3c
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-02-22 11:15:28 +0000

    exercise nr.2

--- Python/python.py
+++ Python/python.py
@@ -16,3 +16,18 @@ result = substrct_or_sum(10, 9)
 print('The result is:', result)
 
 # Exercise nr.2
+# Write a program to iterate the first numbers and in each iteration, print the sum of the current and previous number.
+
+print('Printing the curent and previous number sum in a range (5)')
+previus_num = 0
+# loop from 1 to 5
+for i in range(0, 6):
+    num_sum = previus_num + i
+    print('Curent number', i , 'Previous number', previus_num, 'Sum: ', num_sum)
+    # modify previous number and set it to the current number
+    previus_num = i
+
+
+
+
+
