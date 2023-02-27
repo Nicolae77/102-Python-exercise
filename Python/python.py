e2aaecefd244617256db274dc34078dd328f7591
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-02-27 12:36:26 +0000

    Exercise nr.9

--- Python/python.py
+++ Python/python.py
@@ -109,7 +109,37 @@ print('Python appears', result, 'times')
 # Exercise nr.8
 for num in range(10):
     for i in range(num):
+        print(i, end=" ")
+    print("\n")
+
+for num in range(10):
+    for i in range(num):
         print(num, end=" ")
     print("\n")
     
+
+
+# Exercise nr.9
+# Write a program to check if given number ia a palindrome number.
+# A Palindrome number is a number same after reverse. ex: 343, is the polindrome numbers.
+def palindrome(number):
+    print("original number", number)
+    original_num = number
+    
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+        print(reverse_num)
+
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number palindrome")
+    else:
+        print("Given number is not palindrome")
+
+palindrome(121)
+palindrome(125)
 
