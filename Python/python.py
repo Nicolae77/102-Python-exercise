6ed6be4138128a721dc0387bc7e04f9b837a93d4
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-03-21 08:23:42 +0000

    Exercise nr.100

--- Python/python.py
+++ Python/python.py
@@ -1341,6 +1341,24 @@ def randString(strLength):
 print("Random String is ", randString(10))
 
 
+# Exercise nr.100
+# Generate a random date between given start and end dates
+import random
+import time
+
+def getRandomDate(startDate, endDate ):
+    print("Printing random date between", startDate, " and ", endDate)
+    randomGenerator = random.random()
+    dateFormat = '%m/%d/%Y'
+
+    startTime = time.mktime(time.strptime(startDate, dateFormat))
+    endTime = time.mktime(time.strptime(endDate, dateFormat))
+
+    randomTime = startTime + randomGenerator * (endTime - startTime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+
+print ("Random Date = ", getRandomDate("1/1/2016", "12/12/2018"))
 
 
 # -----------------------------Python Database Exercise--------------------
