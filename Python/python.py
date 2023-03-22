74531bcd335975bef06532283b6b56fe0e4de92e
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-03-22 13:03:50 +0000

    Exercise nr.101

--- Python/python.py
+++ Python/python.py
@@ -1362,4 +1362,32 @@ print ("Random Date = ", getRandomDate("1/1/2016", "12/12/2018"))
 
 
 # -----------------------------Python Database Exercise--------------------
+# Exercise nr.101
+import mysql.connector
+from mysql.connector import cursor
 
+
+def get_connection():
+    connection = mysql.connector.connect(host='127.0.0.1',
+                                         database='python_db',
+                                         user='root',
+                                         password='nicolaecaldarasan17111983')
+    return connection
+
+def close_connection(connection):
+    if connection:
+        connection.close()
+
+def read_database_version():
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT version();")
+        db_version = cursor.fetchone()
+        print("You are connected to MySQL version: ", db_version)
+        close_connection(connection)
+    except (Exception, mysql.connector.Error) as error:
+        print("Error while getting data", error)
+
+print("Question 1: Print Database version")
+read_database_version()
